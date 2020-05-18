4166e5f47ba1e8c49c232eba16488e2553d9ad0d
Rich <richtong888@gmail.com>
2020-05-18 17:54:46 +0800

    first commit

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/richtong/ur5_controll_ws/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/richtong/ur5_controll_ws/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/richtong/ur5_controll_ws/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/richtong/ur5_controll_ws/catkin_ws/src:$ROS_PACKAGE_PATH"
